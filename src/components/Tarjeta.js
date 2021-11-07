74cede18287cfc332cdea7705abf742871f20331
Josue-Melendez-GonzaleZ <melendez.gonzalez.josue@gmail.com>
2021-11-06 21:46:28 -0600

    targetahome vertical
    cambie targeta al home vertical

--- src/components/Tarjeta.js
+++ src/components/Tarjeta.js
@@ -12,7 +12,7 @@ const style = {
 const Tarjeta = (props) => {
     return ( 
           <div className="col s12 m6 l4 center">
-            <div className="card">
+            <div className="card vertical">
               <div className="card-image">
                 <img
                   style={style.cardImg}
