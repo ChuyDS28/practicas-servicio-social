a878676958578b0d934e830e30daffb5fb792fdb
Josue-Melendez-GonzaleZ <melendez.gonzalez.josue@gmail.com>
2021-11-06 21:54:34 -0600

    dimension
    dimension del home card

--- src/components/Tarjeta.js
+++ src/components/Tarjeta.js
@@ -3,16 +3,29 @@ import { Link } from "react-router-dom";
 
 const style = {
   cardImg: {
-    maxHeight: "200px",
+   
     objectFit: "cover",
     filter: "brightness(0.8)",
   },
 };
 
+const cardsize ={
+
+   dimension: {
+
+    mixHeight: "200px",
+
+   }
+
+
+
+}
+
 const Tarjeta = (props) => {
     return ( 
           <div className="col s12 m6 l4 center">
-            <div className="card vertical">
+            <div className="card vertical"
+            cardsize={cardsize.dimension}>
               <div className="card-image">
                 <img
                   style={style.cardImg}
