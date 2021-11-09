ff9a11ef4d51d9e0dd72d231ad667c0e796adb54
Josue-Melendez-GonzaleZ <melendez.gonzalez.josue@gmail.com>
2021-11-08 18:41:07 -0600

    regresar a la normalidad??

--- src/components/TargetaRegistro.js
+++ src/components/TargetaRegistro.js
@@ -1,45 +1,48 @@
 import React from 'react'
-  import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
-  const style = {
-    cardImg: {
-      
-      width: "100%",
-      objectFit: "cover",
-      height: "100%",
+const style = {
+  cardImg: {
+    
+    width: "100%",
+    objectFit: "cover",
+    height: "100%",
 
-    },
-  };
+  },
+};
 
-  const TargetaRegistro = (props) => {
-  return(            
-  <div className="col s12 m6 l6 center">    
-  <div className="card horizontal">
-              <div className="card-image">
-                <img
-                  src={props.pic}
-                  alt=""
-                  style={style.cardImg}
-                  style={{minHeight:"180px"}}
-                />
+const TargetaRegistro = (props) => {
+return(            
+<div className="col s12 m6 l6 center" >    
+ <div className="card horizontal"         style={{maxWidth:"380px"}}>
+            <div className="card-image"  >
+              <img
+                src={props.pic}
+                alt=""
+                style={style.cardImg}
+                style={{minHeight:"300px"}}
+        
+                
+              />
+            </div>
+            <div className="card-stacked">
+              <div className="card-content" style={{maxHeight:"230px"}}
+              >
+                <span className="card-title">{props.user}</span>
+                <p>
+                  {props.description}
+                </p>
               </div>
-              <div className="card-stacked">
-                <div className="card-content" style={{minHeight:"180px"}}>
-                  <span className="card-title">{props.user}</span>
-                  <p>
-                    {props.description}
-                  </p>
-                </div>
-                <div className="card-action">
-                  <Link
-                    to={props.link} 
-                    className="waves-effect waves-light btn-small">
-                    Registrarse
-                  </Link>
-                </div>
+              <div className="card-action">
+                <Link
+                  to={props.link} 
+                  className="waves-effect waves-light btn-small">
+                  Registrarse
+                </Link>
               </div>
             </div>
           </div>
-  );
-  }
-  export default TargetaRegistro;
+        </div>
+);
+}
+export default TargetaRegistro;
