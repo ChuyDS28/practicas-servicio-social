a0fae68d909b2b5acaea947ae7571f298cdc8a23
Josue-Melendez-GonzaleZ <melendez.gonzalez.josue@gmail.com>
2021-11-08 18:38:36 -0600

    Update TargetaRegistro.js

--- src/components/TargetaRegistro.js
+++ src/components/TargetaRegistro.js
@@ -1,48 +1,45 @@
 import React from 'react'
-import { Link } from "react-router-dom";
+  import { Link } from "react-router-dom";
 
-const style = {
-  cardImg: {
-    
-    width: "100%",
-    objectFit: "cover",
-    height: "100%",
+  const style = {
+    cardImg: {
+      
+      width: "100%",
+      objectFit: "cover",
+      height: "100%",
 
-  },
-};
+    },
+  };
 
-const TargetaRegistro = (props) => {
-return(            
-<div className="col s12 m6 l6 center" >    
- <div className="card horizontal"         style={{maxWidth:"380px"}}>
-            <div className="card-image"  >
-              <img
-                src={props.pic}
-                alt=""
-                style={style.cardImg}
-                style={{minHeight:"300px"}}
-        
-                
-              />
-            </div>
-            <div className="card-stacked">
-              <div className="card-content" style={{minHeight:"230px"}}
-              >
-                <span className="card-title">{props.user}</span>
-                <p>
-                  {props.description}
-                </p>
+  const TargetaRegistro = (props) => {
+  return(            
+  <div className="col s12 m6 l6 center">    
+  <div className="card horizontal">
+              <div className="card-image">
+                <img
+                  src={props.pic}
+                  alt=""
+                  style={style.cardImg}
+                  style={{minHeight:"180px"}}
+                />
               </div>
-              <div className="card-action">
-                <Link
-                  to={props.link} 
-                  className="waves-effect waves-light btn-small">
-                  Registrarse
-                </Link>
+              <div className="card-stacked">
+                <div className="card-content" style={{minHeight:"180px"}}>
+                  <span className="card-title">{props.user}</span>
+                  <p>
+                    {props.description}
+                  </p>
+                </div>
+                <div className="card-action">
+                  <Link
+                    to={props.link} 
+                    className="waves-effect waves-light btn-small">
+                    Registrarse
+                  </Link>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-);
-}
-export default TargetaRegistro;
+  );
+  }
+  export default TargetaRegistro;
