cf523f76abe012226fac4b59ef5014c60b495175
Josue-Melendez-GonzaleZ <melendez.gonzalez.josue@gmail.com>
2021-11-09 18:39:54 -0600

    no se que cambios hibo xd

--- src/components/TargetaRegistro.js
+++ src/components/TargetaRegistro.js
@@ -10,13 +10,28 @@ const style = {
   },
 };
 
+const props = {
+propiedades: {
+user:"props",
+description:"props",
+link:"props",
+pic:"props"
+},
+};
+
+const {user, description, link, pic} = props.propiedades
+
+
+
+
+
 const TargetaRegistro = (props) => {
 return(            
 <div className="col s12 m6 l6 center" >    
  <div className="card horizontal " style={{minWidth:"400px"}}>
             <div className="card-image" >
               <img
-                src={props.pic}
+                src={pic}
                 alt=""
                 style={style.cardImg}
               />
@@ -24,14 +39,14 @@ return(
             <div className="card-stacked">
               <div className="card-content" style={{minWidth:"230px"}}
               >
-                <span className="card-title">{props.user}</span>
+                <span className="card-title">{user}</span>
                 <p>
-                  {props.description}
+                  {description}
                 </p>
               </div>
               <div className="card-action" >
                 <Link
-                  to={props.link} 
+                  to={link} 
                   className="waves-effect waves-light btn-small">
                   Registrarse
                 </Link>
