22c052f36babcce7eb114bddfbbbcfe012b2be94
Josue-Melendez-GonzaleZ <melendez.gonzalez.josue@gmail.com>
2021-11-19 20:59:05 -0600

    MODALS TAMAÑO
    SE CAMBIO EL TAMAÑO DE LOS MODALS

--- src/pages/CentrosDVDR/CentrosDVDR.js
+++ src/pages/CentrosDVDR/CentrosDVDR.js
@@ -16,66 +16,85 @@ const CentrosDVDR = () => {
     var elems = document.querySelector(".modal");
     var instance = M.Modal.getInstance(tabs.current);
     instance.open();
-    
   }
 
   const style = {
-    cardImg: {
-      
-      width: "100%",
+    modalImg: {
+      width: "180px",
       objectFit: "cover",
-      height: "100%",
-
+      height: "130px",
     },
-    box: {
-      border: "1px",
-      width: "100%",
-      height: "100%",
-    }
   };
 
-//holacxcxcxfdsfds
-//width="100px" height="100px"
-// style={style.box}
   const ModalHelp = () => {
     return (
-      <div id="modal11" className="modal" ref={tabs}>
-        <div class="box"> 
-        <div class="modal-content row container section" style={{minWidth:"100%"}}>
-          <table>
-            <tr>
-              <td><img src="https://www.encb.ipn.mx/assets/files/encb/img/escudos/logo-ipn.png" style={style.cardImg}/></td>
-              <td><p align="center" style={style.box}><h5><b>Instituto Politécnico Nacional <br/>
-                Secretaría de Innovación e Integración Social<br/>
-                Dirección de Vinculación y Desarrollo Regional</b></h5></p></td>
-              <td><img src="https://www.ipn.mx/assets/files/vinculacion/img/Vinculacion_princ/logo-DVDR.jpg" style={style.cardImg}/></td>
-            </tr>
-          </table>
-          <br />
-          <div>
-         
-            <br /> <br />
+      <div
+        id="modal11"
+        className="modal"
+        ref={tabs}
+        style={{ minWidth: "80%" }}
+      >
+        <div className="row container section" style={{ minWidth: "100%" }}>
+          <div className="modal-content" style={{ minWidth: "100%" }}>
+            <table>
+              <tr>
+                <td>
+                  <img
+                    src="https://www.encb.ipn.mx/assets/files/encb/img/escudos/logo-ipn.png"
+                    style={style.modalImg}
+                  />
+                </td>
+                <td>
+                  <p align="center" style={{ minWidth: "100%" }}>
+                    <h5>
+                      <b>
+                        Instituto Politécnico Nacional <br />
+                        Secretaría de Innovación e Integración Social
+                        <br />
+                        Dirección de Vinculación y Desarrollo Regional
+                      </b>{" "}
+                    </h5>
+                  </p>
+                </td>
+                <td>
+                  <img
+                    src="https://www.ipn.mx/assets/files/vinculacion/img/Vinculacion_princ/logo-DVDR.jpg"
+                    style={style.modalImg}
+                  />
+                </td>
+              </tr>
+            </table>
+            <br />
+            <div>
+              <p align="center">
+                
+                  <h6><b>
+                    Centro de Vinculación y Desarrollo Regional Unidad Cajeme
+                    </b> </h6>
+                  <br />
+               
+              </p>
+            </div>
+            <br />
+            <div className= "center">
+              <iframe
+                width="424"
+                height="238"
+                src="https://www.youtube.com/embed/TOKmS6e_2FY"
+                frameborder="0"
+                allow="autoplay; encrypted-media"
+                allowfullscreen
+              ></iframe>
+            </div>
+
+            <button
+              href="#!"
+              className="modal-close waves-effect waves-green btn-flat right"
+            >
+              Cerrar
+            </button>
           </div>
-          <div className=" ">
-          <h1>MELENDEZ </h1>
-            <br /> <br />
-          </div>
-          <div className="">
-          <h1>Delgadillo</h1>
-            <br /> <br />
-          </div>
-          <div className="">
-          <h1>DIAZ</h1>
-            <br /> <br />
-          </div>
-          <button
-            href="#!"
-            className="modal-close waves-effect waves-green btn-flat right"
-          >
-            Cerrar
-          </button>
         </div>
-      </div>
       </div>
     );
   };
@@ -90,7 +109,6 @@ const CentrosDVDR = () => {
             class="waves-effect waves-light btn modal-trigger"
             onClick={() => cambiodeTexto("Centro1")}
           >
-            
             CENTRO1
           </button>
 
@@ -98,7 +116,6 @@ const CentrosDVDR = () => {
             class="waves-effect waves-light btn modal-trigger"
             onClick={() => cambiodeTexto("Centro2")}
           >
-            
             CENTRO2
           </button>
 
