c143a3709cea1955ebb71b2d8f8b0f4c419773df
Josue-Melendez-GonzaleZ <melendez.gonzalez.josue@gmail.com>
2021-11-19 19:19:03 -0600

    aldo es gay
    aldo

--- src/pages/CentrosDVDR/CentrosDVDR.js
+++ src/pages/CentrosDVDR/CentrosDVDR.js
@@ -19,14 +19,41 @@ const CentrosDVDR = () => {
     
   }
 
+  const style = {
+    cardImg: {
+      
+      width: "100%",
+      objectFit: "cover",
+      height: "100%",
+
+    },
+    box: {
+      border: "1px",
+      width: "100%",
+      height: "100%",
+    }
+  };
+
+//holacxcxcxfdsfds
+//width="100px" height="100px"
+// style={style.box}
   const ModalHelp = () => {
     return (
       <div id="modal11" className="modal" ref={tabs}>
-        <div class="modal-content row container">
-          <h1>Delgadillo </h1>
+        <div class="box"> 
+        <div class="modal-content row container section" style={{minWidth:"100%"}}>
+          <table>
+            <tr>
+              <td><img src="https://www.encb.ipn.mx/assets/files/encb/img/escudos/logo-ipn.png" style={style.cardImg}/></td>
+              <td><p align="center" style={style.box}><h5><b>Instituto Politécnico Nacional <br/>
+                Secretaría de Innovación e Integración Social<br/>
+                Dirección de Vinculación y Desarrollo Regional</b></h5></p></td>
+              <td><img src="https://www.ipn.mx/assets/files/vinculacion/img/Vinculacion_princ/logo-DVDR.jpg" style={style.cardImg}/></td>
+            </tr>
+          </table>
           <br />
           <div>
-          <h1>DIAZ </h1>
+         
             <br /> <br />
           </div>
           <div className=" ">
@@ -49,9 +76,9 @@ const CentrosDVDR = () => {
           </button>
         </div>
       </div>
+      </div>
     );
   };
-
   return (
     <>
       <ModalHelp />
