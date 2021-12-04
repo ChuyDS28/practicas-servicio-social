bd3cd380047664968cdd0e9a5f5297df43fdbe19
Josue-Melendez-GonzaleZ <melendez.gonzalez.josue@gmail.com>
2021-12-03 20:58:16 -0600

    all i've got all do

--- src/pages/CentrosDVDR/CentrosDVDR.js
+++ src/pages/CentrosDVDR/CentrosDVDR.js
@@ -15,115 +15,135 @@ import oaxaca from "..//..//assets/images/CodigosQR/oaxaca.jpg";
 import tampico from "..//..//assets/images/CodigosQR/tampico.png";
 import tijuana from "..//..//assets/images/CodigosQR/tijuana.jpg";
 import veracruz from "..//..//assets/images/CodigosQR/veracruz.jpg";
-import Mapa from "./Mapa"
+import Mapa from "./Mapa";
 
-
-
-
-const data = [{
-        titulo: "Cajeme",
-        link: "https://www.youtube.com/embed/TOKmS6e_2FY",
-        imagenurl: cajeme,
-    },
-    {
-        titulo: "Campeche",
-        link: "https://www.youtube.com/embed/TOKmS6e_2FY",
-        imagenurl: campeche,
-    },
-    {
-        titulo: "Cancun",
-        link: "https://www.youtube.com/embed/TOKmS6e_2FY",
-        imagenurl: cancun,
-    },
+const data = [
+  {
+    titulo: "Cajeme",
+    link: "https://www.youtube.com/embed/TOKmS6e_2FY",
+    imagenurl: cajeme,
+  },
+  {
+    titulo: "Campeche",
+    link: "https://www.youtube.com/embed/TOKmS6e_2FY",
+    imagenurl: campeche,
+  },
+  {
+    titulo: "Cancun",
+    link: "https://www.youtube.com/embed/TOKmS6e_2FY",
+    imagenurl: cancun,
+  },
+  {
+    titulo: "Chihuhua",
+    link: "https://www.youtube.com/embed/mGhpdhj-WqA",
+    imagenurl: chihuahua,
+  },
+  {
+    titulo: "Culiacan",
+    link: "https://www.youtube.com/embed/mpJkOS8T1sI",
+    imagenurl: culiacan,
+  },
+  {
+    titulo: "Durango",
+    link: "https://www.youtube.com/embed/WKDlUFXtuXM",
+    imagenurl: durango,
+  },
+  {
+    titulo: "Mazatlan",
+    link: "https://www.youtube.com/embed/GAta9sP7OMQ",
+    imagenurl: mazatlan,
+  },
+  {
+    titulo: "Monchis",
+    link: "https://www.youtube.com/embed/AWL2oAgY97w",
+    imagenurl: mochis,
+  },
+  {
+    titulo: "Morelia",
+    link: "https://www.youtube.com/embed/4DMc_KxMlfQ",
+    imagenurl: morelia,
+  },
+  {
+    titulo: "Oaxaca",
+    link: "https://www.youtube.com/embed/w5D4fCDkxeM",
+    imagenurl: oaxaca,
+  },
+  {
+    titulo: "Tampico",
+    link: "https://www.youtube.com/embed/GFCy-c7P9Oo",
+    imagenurl: tampico,
+  },
+  {
+    titulo: "Tijuana",
+    link: "https://www.youtube.com/embed/Y5CK0CAcN5I",
+    imagenurl: tijuana,
+  },
+  {
+    titulo: "Veracruz",
+    link: "https://www.youtube.com/embed/lZYyoqtKtuw",
+    imagenurl: veracruz,
+  },
 ];
 
+
+
 const CentrosDVDR = () => {
+  
     const [centro, setCentro] = useState("Centro");
     const [link, setLink] = useState("Link");
     const [imagenurl, setimagenUrl] = useState("URL");
     const [manejadorModal, setmanejadorModal] = useState(false);
-
-    function cambiodeTexto(texto) {
-        setCentro(texto);
-        setmanejadorModal(true);
-    }
-
-    function cambiodeVinculo(vinculo) {
-        setLink(vinculo);
-        setmanejadorModal(true);
-    }
-
-    function cambiodeURL(vinculo) {
-        setimagenUrl(vinculo);
-        setmanejadorModal(true);
-    }
-
-    function abrirModal(titulo, youtube, codigo) {
-        setCentro(titulo);
-        setLink(youtube);
-        setimagenUrl(codigo);
-        setmanejadorModal(true);
-    }
-    return ( <
-        >
-
-
-        <
-        Modal open = { manejadorModal }
-        fnCloseModal = {
-            () => setmanejadorModal(false) } >
-        <
-        InformacionDelCentro unidad = { centro }
-        vinculo = { link }
-        url = { imagenurl }
-        />{" "} <
-        /Modal>{" "} <
-        div >
-        <
-        header > < /header>{" "} <
-        main >
-        <
-        h1 > { centro } < /h1>{" "} {
-            data.map((centro) => ( <
-                button class = "waves-effect waves-light btn modal-trigger"
-                onClick = {
-                    () =>
-                    abrirModal(centro.titulo, centro.link, centro.imagenurl)
-                } >
-                { centro.titulo } { " " } <
-                /button>
-            ))
-        } <
-        button class = "waves-effect waves-light btn modal-trigger"
-        onClick = {
-            () =>
-            abrirModal(
+  
+  function abrirModal(titulo, youtube, codigo) {
+    setCentro(titulo);
+    setLink(youtube);
+    setimagenUrl(codigo);
+    setmanejadorModal(true);
+  }
+  return (
+    <>
+      <Modal
+        open={manejadorModal}
+        fnCloseModal={() => setmanejadorModal(false)}
+      >
+        <InformacionDelCentro unidad={centro} vinculo={link} url={imagenurl} />{" "}
+      </Modal>{" "}
+      <div>
+        <header> </header>
+        <main>
+          <h1> {centro} </h1>
+          {data.map((centro) => (
+            <button
+              class="waves-effect waves-light btn modal-trigger"
+              onClick={() =>
+                abrirModal(centro.titulo, centro.link, centro.imagenurl)
+              }
+            >
+              {centro.titulo}
+            </button>
+          ))}
+          <button
+            class="waves-effect waves-light btn modal-trigger"
+            onClick={() =>
+              abrirModal(
                 "Centro de Vinculación y Desarrollo Regional Unidad Cajeme",
                 "https://www.youtube.com/embed/TOKmS6e_2FY",
                 cajeme
-            )
-        } >
-        prueba { " " } <
-        /button>{" "} <
-        /main>{" "}
-
-
-
-        <
-        Mapa fnabrirmodal = { abrirModal }
-
-
-
+              )
+            }
+          >
+            prueba{""}
+          </button>
+        </main>
+        <Mapa 
+        fnabrirmodal={abrirModal} 
+        
         />
-
-
-
-        <
-        Footer / >
-        <
-        /div>; { " " } <
-        />
-    );
+        <Footer />
+      </div>
+      ;
+    </>
+  );
 };
 
 export default CentrosDVDR;
