2bfcc905c75eac77a4bc5c4761b1bd020731d102
aldodm1907 <adiazm1904@alumno.ipn.mx>
2021-12-09 17:17:08 -0600

    dasdasa

--- src/pages/CentrosDVDR/CentrosDVDR.js
+++ src/pages/CentrosDVDR/CentrosDVDR.js
@@ -18,102 +18,102 @@ import veracruz from "..//..//assets/images/CodigosQR/veracruz.jpg";
 import Mapa from "./Mapa";
 
 const data = {
-    ucajeme: {
-        titulo: "Cajeme",
-        link: "https://www.youtube.com/embed/TOKmS6e_2FY",
-        imagenurl: cajeme,
-    },
-    ucampenche: {
-        titulo: "Campeche",
-        link: "https://www.youtube.com/embed/TOKmS6e_2FY",
-        imagenurl: campeche,
-    },
-    ucancun: {
-        titulo: "Cancun",
-        link: "https://www.youtube.com/embed/TOKmS6e_2FY",
-        imagenurl: cancun,
-    },
-    uchihuahua: {
-        titulo: "Chihuhua",
-        link: "https://www.youtube.com/embed/mGhpdhj-WqA",
-        imagenurl: chihuahua,
-    },
-    uculiacan: {
-        titulo: "Culiacan",
-        link: "https://www.youtube.com/embed/mpJkOS8T1sI",
-        imagenurl: culiacan,
-    },
-    udurango: {
-        titulo: "Durango",
-        link: "https://www.youtube.com/embed/WKDlUFXtuXM",
-        imagenurl: durango,
-    },
-    umazatlan: {
-        titulo: "Mazatlan",
-        link: "https://www.youtube.com/embed/GAta9sP7OMQ",
-        imagenurl: mazatlan,
-    },
-    umochis: {
-        titulo: "Monchis",
-        link: "https://www.youtube.com/embed/AWL2oAgY97w",
-        imagenurl: mochis,
-    },
-    umorelia: {
-        titulo: "Morelia",
-        link: "https://www.youtube.com/embed/4DMc_KxMlfQ",
-        imagenurl: morelia,
-    },
-    uoaxaca: {
-        titulo: "Oaxaca",
-        link: "https://www.youtube.com/embed/w5D4fCDkxeM",
-        imagenurl: oaxaca,
-    },
-    utampico: {
-        titulo: "Tampico",
-        link: "https://www.youtube.com/embed/GFCy-c7P9Oo",
-        imagenurl: tampico,
-    },
-    utijuana: {
-        titulo: "Tijuana",
-        link: "https://www.youtube.com/embed/Y5CK0CAcN5I",
-        imagenurl: tijuana,
-    },
-    uveracruz: {
-        titulo: "Veracruz",
-        link: "https://www.youtube.com/embed/lZYyoqtKtuw",
-        imagenurl: veracruz,
-    },
+  ucajeme: {
+    titulo: "Cajeme",
+    link: "https://www.youtube.com/embed/TOKmS6e_2FY",
+    imagenurl: cajeme,
+  },
+  ucampenche: {
+    titulo: "Campeche",
+    link: "https://www.youtube.com/embed/TOKmS6e_2FY",
+    imagenurl: campeche,
+  },
+  ucancun: {
+    titulo: "Cancun",
+    link: "https://www.youtube.com/embed/TOKmS6e_2FY",
+    imagenurl: cancun,
+  },
+  uchihuahua: {
+    titulo: "Chihuhua",
+    link: "https://www.youtube.com/embed/mGhpdhj-WqA",
+    imagenurl: chihuahua,
+  },
+  uculiacan: {
+    titulo: "Culiacan",
+    link: "https://www.youtube.com/embed/mpJkOS8T1sI",
+    imagenurl: culiacan,
+  },
+  udurango: {
+    titulo: "Durango",
+    link: "https://www.youtube.com/embed/WKDlUFXtuXM",
+    imagenurl: durango,
+  },
+  umazatlan: {
+    titulo: "Mazatlan",
+    link: "https://www.youtube.com/embed/GAta9sP7OMQ",
+    imagenurl: mazatlan,
+  },
+  umochis: {
+    titulo: "Monchis",
+    link: "https://www.youtube.com/embed/AWL2oAgY97w",
+    imagenurl: mochis,
+  },
+  umorelia: {
+    titulo: "Morelia",
+    link: "https://www.youtube.com/embed/4DMc_KxMlfQ",
+    imagenurl: morelia,
+  },
+  uoaxaca: {
+    titulo: "Oaxaca",
+    link: "https://www.youtube.com/embed/w5D4fCDkxeM",
+    imagenurl: oaxaca,
+  },
+  utampico: {
+    titulo: "Tampico",
+    link: "https://www.youtube.com/embed/GFCy-c7P9Oo",
+    imagenurl: tampico,
+  },
+  utijuana: {
+    titulo: "Tijuana",
+    link: "https://www.youtube.com/embed/Y5CK0CAcN5I",
+    imagenurl: tijuana,
+  },
+  uveracruz: {
+    titulo: "Veracruz",
+    link: "https://www.youtube.com/embed/lZYyoqtKtuw",
+    imagenurl: veracruz,
+  },
 };
 
 const CentrosDVDR = () => {
-    const [estadoSeleccionado, setEstadoSeleccionado] = useState({ titulo: "", link: "", imagenurl: "" });
-    const [manejadorModal, setmanejadorModal] = useState(false);
+  const [estadoSeleccionado, setEstadoSeleccionado] = useState({
+    titulo: "",
+    link: "",
+    imagenurl: "",
+  });
+  const [manejadorModal, setmanejadorModal] = useState(false);
 
-    function abrirModal(titulo, youtube, codigo) {
-        setEstadoSeleccionado({
-            titulo: titulo,
-            link: youtube,
-            imagenurl: codigo
-        })
-        setmanejadorModal(true);
-    }
-    return ( <
-        >
-        <
-        Modal open = { manejadorModal }
-        fnCloseModal = {
-            () => setmanejadorModal(false) } >
-        <
-        InformacionDelCentro data = { estadoSeleccionado }
-        />{" "} <
-        /Modal>{" "} <
-        div >
-        <
-        header > < /header> <
-        main >
-        <
-        h1 > { estadoSeleccionado.titulo } < /h1> {
-            /*data.entries.map((centro) => ( 
+  function abrirModal(titulo, youtube, codigo) {
+    setEstadoSeleccionado({
+      titulo: titulo,
+      link: youtube,
+      imagenurl: codigo,
+    });
+    setmanejadorModal(true);
+  }
+  return (
+    <>
+      <Modal
+        open={manejadorModal}
+        fnCloseModal={() => setmanejadorModal(false)}
+      >
+        <InformacionDelCentro data={estadoSeleccionado} />{" "}
+      </Modal>{" "}
+      <div>
+        <header> </header>{" "}
+        <main>
+          <h1> {estadoSeleccionado.titulo} </h1>{" "}
+          {/*data.entries.map((centro) => ( 
                         <button
                           class="waves-effect waves-light btn modal-trigger"
                           onClick={() =>
@@ -121,30 +121,30 @@ const CentrosDVDR = () => {
                           }>
                           {centro.titulo}
                         </button>
-                        ))*/
-        } <
-        /main> <
-        Mapa data = { data }
-        fnabrirmodal = { abrirModal }
-        imagen1 = { cajeme }
-        imagen2 = { campeche }
-        imagen3 = { cancun }
-        imagen4 = { chihuahua }
-        imagen5 = { culiacan }
-        imagen6 = { durango }
-        imagen7 = { mazatlan }
-        imagen8 = { mochis }
-        imagen9 = { morelia }
-        imagen10 = { oaxaca }
-        imagen11 = { tampico }
-        imagen12 = { tijuana }
-        imagen13 = { veracruz }
-        /> <
-        Footer / >
-        <
-        /div>; <
-        />
-    );
+                        ))*/}{" "}
+        </main>{" "}
+        <Mapa
+          data={data}
+          fnabrirmodal={abrirModal}
+          imagen1={cajeme}
+          imagen2={campeche}
+          imagen3={cancun}
+          imagen4={chihuahua}
+          imagen5={culiacan}
+          imagen6={durango}
+          imagen7={mazatlan}
+          imagen8={mochis}
+          imagen9={morelia}
+          imagen10={oaxaca}
+          imagen11={tampico}
+          imagen12={tijuana}
+          imagen13={veracruz}
+        />{" "}
+        <Footer />
+      </div>
+      ;{" "}
+    </>
+  );
 };
 
 export default CentrosDVDR;
