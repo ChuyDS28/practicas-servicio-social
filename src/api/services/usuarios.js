c2676016d6d25a4b2499c679bc944646160773f9
ChuyDS28 <delgajesus6@gmail.com>
2022-01-20 21:48:04 -0600

    BOOM

--- src/api/services/usuarios.js
+++
@@ -1,9 +0,0 @@
-import axios from "../axios";
-
-export function obtenerUsuario(id) {
-  return axios.get(`/usuarios/${id}`);
-}
-
-export function habilitarUsuario(id) {
-    return axios.patch(`/usuarios/${id}/activar`);
-  }
