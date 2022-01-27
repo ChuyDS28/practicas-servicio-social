a10f5ec82cd4110db717aa0c5cf2c923e1f5ac74
Josue-Melendez-GonzaleZ <melendez.gonzalez.josue@gmail.com>
2022-01-26 18:27:26 -0600

    pongan

--- src/pages/admin/AdministrarUsuarios/AgregarUsuario.js
+++ src/pages/admin/AdministrarUsuarios/AgregarUsuario.js
@@ -2,61 +2,53 @@ import React from "react";
 
 const AgregarUsuario = () => {
   return (
-    <div className="modal-content  ">
+    <>
       <div className="teal white-text section center">
         <h5 className="center ">Agregar nuevo Usuario</h5>
       </div>
-      <br />
-      <div className="row">
-        <div className="col s12 m12 l7 xl7">
-          <p>
-            <strong>Nombre:&nbsp;&nbsp;</strong>Pedro Díaz Coronel
-          </p>
-        </div>
 
-        <div className="col s12 m12 l4 xl4">
-          <p>
-            <strong>Tipo de usuario:&nbsp;&nbsp;</strong>Subdirector
-          </p>
+      <div className="input-field col s12">
+        <input id="nombreUsuario" type="text" required />
+        <label htmlFor="nombreCurso">Nombre Completo</label>
+      </div>
+      <div className="input-field col s12">
+        <div className="input-field col s6">
+          <input id="apellidoP" type="text" required />
+          <label htmlFor="nombreArea">Apellido Paterno</label>
         </div>
-
-        <div className="col s12 l7">
-          <p>
-            <strong>Escuela, unidad o centro:&nbsp;&nbsp;</strong>
-            Juan de Dios Bátiz
-          </p>
-        </div>
-        <div className="col s12 m4 l5">
-          <p>
-            <strong>Cargo:&nbsp;&nbsp;</strong>Principal
-          </p>
-        </div>
-        <div className="col s12 m7 xl7">
-          <p>
-            <strong>Fecha de registro:&nbsp;&nbsp;</strong>16/11/2021
-          </p>
-        </div>
-        <div className="col s12 m5 xl5">
-          <p>
-            <strong>Fecha de inactividad:&nbsp;&nbsp;</strong>17/11/2021
-          </p>
-        </div>
-        <div className="col s12 m7 xl7">
-          <p>
-            <strong>Número de telefono:&nbsp;&nbsp;</strong>5570293846
-          </p>
-        </div>
-        <div className="col s12 m5 xl5">
-          <p>
-            <strong>Extensión:&nbsp;&nbsp;</strong>551728
-          </p>
+        <div className="input-field col s6">
+          <input id="apellidoM" type="text" data-length="13" required />
+          <label htmlFor="nombreCurso">Apellido Materno</label>
         </div>
       </div>
-      <button className=" modal-close waves-effect waves-green btn-flat right">
+
+      <div className="file-field input-field col s12">
+        <div className="file-path-wrapper">
+          <input
+            className="file-path validate"
+            type="text"
+            placeholder="Adjuntar CV del Instructor en formato PDF"
+          />
+        </div>
+      </div>
+      <div className="input-field col s12">
+
+
+        <div className="file-field input-field col s12">
+          <div className="file-path-wrapper">
+            <input
+              className="file-path validate"
+              type="text"
+              placeholder="Documentos probatorios"
+            />
+          </div>
+        </div>
+      </div>
+
+      <button className="modal-close waves-effect waves-green btn-flat right">
         Cerrar
       </button>
-      <br />
-    </div>
+    </>
   );
 };
 
