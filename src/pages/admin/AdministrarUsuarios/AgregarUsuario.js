786008c4fb2d9e15b9e4a79fee52fbd0c265d9ef
Josue-Melendez-GonzaleZ <melendez.gonzalez.josue@gmail.com>
2022-02-10 21:00:35 -0600

    asd

--- src/pages/admin/AdministrarUsuarios/AgregarUsuario.js
+++ src/pages/admin/AdministrarUsuarios/AgregarUsuario.js
@@ -1,21 +1,15 @@
-<<<<<<< HEAD
 import React, { useEffect, useRef, useState } from "react";
-=======
-import React, { useEffect, useRef} from "react";
->>>>>>> 772afc679ac8bd1f5a20ee6ef4e38b73d9a06fb1
 import M from "materialize-css/dist/js/materialize.min.js";
+import { obtenerUnidades } from "../../../api/services/unidadAcademica";
+import { registrarUsuario } from "../../../api/services/registro";
+import { useNavigate, useParams } from "react-router-dom";
 
 const AgregarUsuario = () => {
-<<<<<<< HEAD
   let navigate = useNavigate();
   let { userType } = useParams();
-=======
-
->>>>>>> 772afc679ac8bd1f5a20ee6ef4e38b73d9a06fb1
   const tabsRef = useRef(null);
 
   useEffect(() => {
-<<<<<<< HEAD
     let elems = document.querySelectorAll(".tabs");
     M.Tabs.init(elems, { duration: 30 });
   });
@@ -116,38 +110,6 @@ const AgregarUsuario = () => {
             </div>
 
 
-=======
-    
-    let elems = document.querySelectorAll(".tabs", ".select");
-    M.Tabs.init(elems, {duration: 30} );
-   
-  });
-
-  return (
-    <div className="modal-content">
-      <div className="teal white-text section center">
-        <h5 className="center ">Agregar nuevo Usuario</h5>
-      </div>
-      <div class="row">
-        <div class="col s12">
-          <ul class="tabs">
-            <li class="tab col s6">
-              <a href="#director">Director</a>
-            </li>
-            <li class="tab col s6">
-              <a class="active" href="#subdirector">
-                Subdirector
-              </a>
-            </li>
-          </ul>
-        </div>
-        
-        <div id="director" class="col s12 ">
-      
-            <div class="row">
-              <form class="col s12">
-                <div class="row">
->>>>>>> 772afc679ac8bd1f5a20ee6ef4e38b73d9a06fb1
 
             <div id="director" class="col s12 ">
               <div class="row">
@@ -255,7 +217,6 @@ const AgregarUsuario = () => {
                 </form>
               </div>
             </div>
-<<<<<<< HEAD
             <div id="subdirector" class="col s12 ">
               <div class="row">
                 <form class="col s12">
@@ -350,58 +311,6 @@ const AgregarUsuario = () => {
                       <input type="text" placeholder="Telefono" id="Telefono" />
                       <label for="Telefono">Telefono:</label>
                     </div>
-=======
-        </div>
-        <div id="subdirector" class="col s12 "> 
-        
-        <div class="row">
-              <form class="col s12">
-                <div class="row">
-
-                <div class="input-field col s4">
-                 <input type="text" placeholder="Ingresa tu nombre" id="nombre"/>
-                  <label for="nombre">Nombre:</label> 
-               </div>
-               <div class="input-field col s4">
-                 <input type="text" placeholder="Ingresa tu apelldio paterno" id="apellido1"/>
-                  <label for="apellido1">Apellido Paterno:</label> 
-               </div>
-               <div class="input-field col s4">
-                 <input type="text" placeholder="Ingresa tu apelldio materno" id="apellido2"/>
-                  <label for="apellido2">Apellido Materno:</label> 
-               </div>
-               <div class="input-field col s6">
-                 <input type="text" placeholder="Ingresa tu correo electronico" id="correo"/>
-                  <label for="correo">Correo:</label> 
-               </div>
-               <div class="input-field col s6">
-                 <input type="text" placeholder="Ingresa tu nombre de usuario" id="usuario"/>
-                  <label for="usuario">Usuario:</label> 
-               </div>
-               <div class="input-field col s6">
-                 <input type="text" placeholder="Crea una contraseña" id="contraseña"/>
-                  <label for="contraseña">Contraseña:</label> 
-               </div>
-               <div class="input-field col s6">
-                 <input type="text" placeholder="Ingresa tu No. de empleado" id="numemp"/>
-                  <label for="numemp">Numero de Empleado:</label> 
-               </div>
-               
-               
-               
-               
-               
-               <div class="input-field col s6">
-                 <selec>
-                 <option value="" disabled selected>Choose your option</option>
-                 <option value="1">Option 1</option>
-                <option value="2">Option 2</option>
-                <option value="3">Option 3</option>
-                 </selec>
-                  <label for="unidad">Unidad academica:</label> 
-               </div>
-
->>>>>>> 772afc679ac8bd1f5a20ee6ef4e38b73d9a06fb1
 
                     <button
                       type="submit"
@@ -524,13 +433,6 @@ const AgregarUsuario = () => {
       <div className="col s12 m12" >
         <SignupForm />
       </div>
-<<<<<<< HEAD
-=======
-      <button className=" modal-close waves-effect waves-green btn-flat right">
-        Cerrar
-      </button>
-      <br />
->>>>>>> 772afc679ac8bd1f5a20ee6ef4e38b73d9a06fb1
     </div>
   );
 };
