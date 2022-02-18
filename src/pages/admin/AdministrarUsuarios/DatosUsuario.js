c345f83181029744e3c1886d3fe6f736b90c303d
ChuyDS28 <delgajesus6@gmail.com>
2022-02-17 20:05:44 -0600

    prueba

--- src/pages/admin/AdministrarUsuarios/DatosUsuario.js
+++ src/pages/admin/AdministrarUsuarios/DatosUsuario.js
@@ -19,12 +19,14 @@ const DatosUsuario = (props) => {
               <strong>&nbsp;&nbsp;</strong>{props.user.segundoApellido}              
             </p>
           </div>
-
+          <br/>
           <div className="col s12 m12 l4 xl4">
             <p>
               <strong>Tipo de usuario:&nbsp;&nbsp;</strong>{props.user.rol}
             </p>
           </div>
+          <br/>
+          <br/>
           {props.user.unidadAcademica && 
           <div className="col s12 l7">
           <p>
@@ -32,51 +34,63 @@ const DatosUsuario = (props) => {
           </p>
         </div>
           }
+          <br/>          
           <div className="col s12 m4 l5">
             <p>
               <strong>Cargo:&nbsp;&nbsp;</strong>{props.user.unidad}
             </p>
           </div>
+          <br/>
           <div className="col s12 l7">
             <p>
               <strong>Usermane:&nbsp;&nbsp;</strong>{props.user.username}
             </p>
           </div>
+          <br/>
           <div className="col s12 m4 l5">
             <p>
               <strong>Correo institucional:&nbsp;&nbsp;</strong>{props.user.correoInstitucional}
             </p>
           </div>
+          <br/>
+          <br/>
           <div className="col s12 m4 l5">
             <p>
               <strong>id:&nbsp;&nbsp;</strong>{props.user.id}
             </p>
           </div>
+          <br/>
           <div className="col s12 m4 l5">
             <p>
               <strong>Correo personal:&nbsp;&nbsp;</strong>{props.user.correoPersonal}
             </p>
           </div>
+          <br/>
           <div className="col s12 m7 xl7">
             <p>
               <strong>Fecha de registro:&nbsp;&nbsp;</strong>{props.user.creado}
             </p>
           </div>
+          <br/>
           <div className="col s12 m5 xl5">
             <p>
               <strong>Fecha de inactividad:&nbsp;&nbsp;</strong>{props.user.fechaUltimaConexion}
             </p>
           </div>
+          <br/>
           <div className="col s12 m7 xl7">
             <p>
               <strong>Número de telefono:&nbsp;&nbsp;</strong>{props.user.Telefono}
             </p>
           </div>
+          <br/>
+          <br/>
           <div className="col s12 m5 xl5">
             <p>
               <strong>Extensión:&nbsp;&nbsp;</strong>{props.user.Extencion}
             </p>
           </div>
+          <br/>
         </div>
         <button className="light-blue lighten-2 white-text modal-close waves-effect waves-green btn-flat right">
           Cerrar
