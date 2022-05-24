e4d0cf2860ea503be752716da2f3643c1a7c48b2
ChuyDS28 <delgajesus6@gmail.com>
2022-05-24 18:37:33 -0500

    x

---
+++ src/api/services/admin/solicitudes.js
@@ -0,0 +1,18 @@
+import axios from "../../axios";
+export function obtenerSolicitudes(pag) {
+    return axios.get(`/solicitudes`, {
+      params: { pagina: pag, tam: 50 },
+    });
+  }
+  export function obtenerSolicitudesSinRevisor(pag) {
+    return axios.get(`/solicitudes/sinRevisor`, {
+      params: { pagina: pag, tam: 50 },
+    });
+  }
+export function asignarRevisor(idSolicitud, idRevisor) {
+    return axios.patch(`solicitudes/${idSolicitud}`, {
+      idRevisor: idRevisor,
+    
+      
+    });
+  }
